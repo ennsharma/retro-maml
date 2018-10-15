c48079a5f8b5f2f6837d6c4c473d8c4b7ad1d1c4
Nikhil <ennsharma@berkeley.edu>
2018-10-14 22:29:55 -0700

    More inits

---
+++ maml.py
@@ -0,0 +1,17 @@
+import numpy as numpy
+import tensorflow as tf
+
+class MetaLearner(object):
+
+	def __init__(self):
+		self._sess = self._setup_graph()
+
+	def _setup_placeholders(self):
+		return
+
+	def _setup_graph(self):
+
+		sess = tf.Session()
+
+		return sess
+
